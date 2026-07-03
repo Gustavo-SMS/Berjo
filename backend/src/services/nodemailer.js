9a3189affa2b82e849540a6803af641286327d19
Gustavo Silva <guh600@gmail.com>
2026-07-02 21:49:36 -0300

    Atualiza configuração do nodemailer com novos timeouts;

--- backend/src/services/nodemailer.js
+++ backend/src/services/nodemailer.js
@@ -3,6 +3,10 @@ const nodemailer = require('nodemailer')
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
+    secure: false,
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
